8f5e83848fe2f168333019343d6739da9430842c
aleena-git-08 <aleenamarydaruja@gmail.com>
2026-07-02 14:30:46 +0530

    display sentiment analysis results for news headlines

--- lambda/sentiment_test.py
+++ lambda/sentiment_test.py
@@ -15,10 +15,14 @@ for article in data["results"][:5]:
     title = article["title"]
 
     score = TextBlob(title).sentiment.polarity
-    
+
     if score > 0:
         sentiment = "Positive"
     elif score < 0:
         sentiment = "Negative"
     else:
         sentiment = "Neutral"
+
+    print(title)
+    print("Sentiment:", sentiment)
+    print("-" * 50)
