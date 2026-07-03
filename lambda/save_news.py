6ceb3e63a570d3624e38bd2c65538c3e738fbf93
aleena-git-08 <aleenamarydaruja@gmail.com>
2026-07-03 12:05:01 +0530

    fetch latest news from NewsData API

--- lambda/save_news.py
+++ lambda/save_news.py
@@ -7,3 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+
+url = f"https://newsdata.io/api/1/latest?apikey={API_KEY}&language=en"
+
+data = requests.get(url).json()
